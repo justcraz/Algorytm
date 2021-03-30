5c02849aee0169393e08a81fc1b2d8fd72bba728
Alexandr Grigoraschuk <64975380+justcraz@users.noreply.github.com>
2021-03-30 21:14:25 +0300

    Update lab2ex3.c

--- lab2ex3.c
+++ lab2ex3.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 int main(void) {
-float a, y;
-printf("Задайте три числа:\n"); //Введення чисел
+float a, y, x;
+printf("Задайте два числа:\n"); //Введення чисел
 
 printf("\nВведіть число a: ");
 scanf("%f", &a);
 
 printf("Введіть число y: ");
 scanf("%f", &y);
-
-y=(a*(sqrt(pow(y,2)-2*a))); //Обчислення
-
+x=(a*(sqrt(pow(y,2)-2*a))); //Обчислення
+  
 printf("Число a = %.f\n", a); //Виведення результатів
-printf("Число b = %.f\n", y);
+printf("Число y = %.f\n", y);
 
-printf("Відповідь: ");
-printf("y = %.2f", y);
+printf("x = %.2f", x);
 return 0;
 }
